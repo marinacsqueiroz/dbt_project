754a6e1eb2cde4dd6ef0cf901c70d17867645535
Marina Queiroz <marina.queiroz@grupoorion.com.br>
2025-10-01 19:31:08 +0200

    ✨ dim and stg concepts

--- jaffle_shop/models/marts/dim_customers.sql
+++ jaffle_shop/models/marts/dim_customers.sql
@@ -1,23 +1,13 @@
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from workspace.jaffle_shop.customers
-
+    select * from {{ref('stg_customers')}}
+    
 ),
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
+        select * from {{ref('stg_orders')}}
 
-    from workspace.jaffle_shop.orders
 
 ),
 
