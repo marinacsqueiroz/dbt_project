6fc827ae2e33cc7b9eadeb195d4f8d74c38ce468
Marina Queiroz <marina.queiroz@grupoorion.com.br>
2025-10-05 10:55:43 +0200

    ✨ YML file and source config. Alert freshness

--- jaffle_shop/models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ jaffle_shop/models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -1,7 +1,6 @@
 select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from workspace.jaffle_shop.orders
+    id as order_id,
+    user_id as customer_id,
+    order_date,
+    status
+from {{ source('jaffle_shop', 'orders') }}
